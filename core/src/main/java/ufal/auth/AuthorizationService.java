ac23028460be2a46fb326f12f90d7024a9270605
Davi Alexandre <deltreertl@gmail.com>
2024-10-17 02:09:20 -0300

    fix

--- core/src/main/java/ufal/auth/AuthorizationService.java
+++ core/src/main/java/ufal/auth/AuthorizationService.java
@@ -4,11 +4,9 @@ import ufal.Usuario;
 
 public class AuthorizationService {
   public static boolean hasPermission(Usuario user, Effect effect, Action action) {
-    for (Role role : user.getRoles()) {
-      for (Permission permission : role.getPermissions()) {
-        if (permission.getEffect() == effect && permission.getAction() == action)
-          return true;
-      }
+    for (Permission permission : user.getRole().getPermissions()) {
+      if (permission.getEffect() == effect && permission.getAction() == action)
+        return true;
     }
     return false;
   }
