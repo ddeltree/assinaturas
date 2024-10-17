e26324d65a4b992ba8f0d77a12f2050e529ef8bd
Davi Alexandre <deltreertl@gmail.com>
2024-10-16 22:48:04 -0300

    métodos do usuário

--- core/src/main/java/ufal/Usuario.java
+++ core/src/main/java/ufal/Usuario.java
@@ -2,27 +2,70 @@ package ufal;
 
 import java.util.HashSet;
 import java.util.Set;
+import java.util.UUID;
 
 import ufal.auth.Role;
 
 public class Usuario {
-  private String uid;
-  private String name;
+  private final String uid;
+  private String nome;
   private String email;
-  private String password;
+  private String senha;
   private final Set<Role> roles = new HashSet<>();
 
-  public Usuario(String email, String password) {
+  public Usuario(String email, String senha) {
     super();
-    this.email = email;
-    this.password = password;
+    this.email = email; // TODO validar
+    this.senha = senha; // TODO validar
+    UUID uuid = UUID.randomUUID();
+    this.uid = uuid.toString();
   }
 
   public Set<Role> getRoles() {
     return roles;
   }
 
-  public boolean verificarLogin(String email, String password) {
-    return this.email.equals(email) && this.password.equals(password);
+  public boolean verificarLogin(String email, String senha) {
+    return this.email.equals(email) && this.senha.equals(senha);
+  }
+
+  public String getNome() {
+    return nome;
+  }
+
+  public String getUID() {
+    return uid;
+  }
+
+  public Usuario atualizarNome(String novoNome) {
+    if (novoNome.isEmpty())
+      throw new IllegalArgumentException("O nome de usuário não pode ser vazio!");
+    this.nome = novoNome;
+    return this;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public Usuario atualizarEmail(String email) {
+    // TODO validar email
+    this.email = email;
+    return this;
+  }
+
+  public void mudarSenha(String novaSenha) {
+    // TODO validar senha
+    this.senha = novaSenha;
+  }
+
+  private boolean isSenhaValida(String senha) {
+    // TODO
+    return true;
+  }
+
+  private boolean isEmailValido(String email) {
+    // TODO
+    return true;
   }
 }
